bc37bab72b6e6a08978d3bc4195b8d284f605a29
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2023-04-05 09:46:28 -0400

    Update MangaThemesiaGenerator.kt

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangathemesia/MangaThemesiaGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangathemesia/MangaThemesiaGenerator.kt
@@ -14,6 +14,7 @@ class MangaThemesiaGenerator : ThemeSourceGenerator {
 
     override val sources = listOf(
         SingleLang("Luminous Scans", "https://www.luminousscans.com", "en", overrideVersionCode = 1),
+        SingleLang("Tecno Scan", "https://tecnoscann.com", "es", overrideVersionCode = 1),
     )
 
     companion object {
