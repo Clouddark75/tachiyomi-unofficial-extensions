13f5cbcb6ee6ff21eec88f3c1958fe84a2231a22
Basara-Hatake <53382992+Basara-Hatake@users.noreply.github.com>
2023-04-14 20:29:20 +0200

    fix

--- multisrc/overrides/mangathemesia/luminousscans/src/LuminousScans.kt
+++ multisrc/overrides/mangathemesia/luminousscans/src/LuminousScans.kt
@@ -1,5 +1,36 @@
 package eu.kanade.tachiyomi.extension.en.luminousscans
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.SManga
+import org.jsoup.nodes.Element
 
-class LuminousScans : MangaThemesia("Luminous Scans", "https://www.luminousscans.com", "en", mangaUrlDirectory = "/series")
+class LuminousScans : MangaThemesia("Luminous Scans", "https://www.luminousscans.com", "en", mangaUrlDirectory = "/series") {
+    override fun searchMangaFromElement(element: Element): SManga {
+        val manga = super.searchMangaFromElement(element)
+
+        val response = client.newCall(GET(baseUrl + manga.url, headers)).execute().body.string()
+
+        val postid = response
+            .substringAfter("post_id:")
+            .substringBefore("}")
+
+        manga.url = "$mangaUrlDirectory?p=$postid"
+
+        return manga
+    }
+
+//    override fun chapterFromElement(element: Element): SChapter {
+//        val chapter = super.chapterFromElement(element)
+//
+//        val response = client.newCall(GET(baseUrl + chapter.url, headers)).execute().body.string()
+//
+//        val postid = response
+//            .substringAfter("var chapter_id = ")
+//            .substringBefore(";")
+//
+//        chapter.url = "/?p=$postid"
+//
+//        return chapter
+//    }
+}
