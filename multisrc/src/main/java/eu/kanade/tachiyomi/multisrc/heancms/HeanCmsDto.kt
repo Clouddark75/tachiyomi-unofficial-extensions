fb755f041f6255f3b03206423166f1d6cec0e3d7
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2023-08-06 12:38:00 -0400

    Add files via upload

---
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/heancms/HeanCmsDto.kt
@@ -0,0 +1,143 @@
+package eu.kanade.tachiyomi.multisrc.heancms
+
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.jsoup.Jsoup
+import java.text.SimpleDateFormat
+
+@Serializable
+data class HeanCmsQuerySearchDto(
+    val data: List<HeanCmsSeriesDto> = emptyList(),
+    val meta: HeanCmsQuerySearchMetaDto? = null,
+)
+
+@Serializable
+data class HeanCmsQuerySearchMetaDto(
+    @SerialName("current_page") val currentPage: Int,
+    @SerialName("last_page") val lastPage: Int,
+) {
+
+    val hasNextPage: Boolean
+        get() = currentPage < lastPage
+}
+
+@Serializable
+data class HeanCmsSearchDto(
+    val description: String? = null,
+    @SerialName("series_slug") var slug: String,
+    @SerialName("series_type") val type: String,
+    val title: String,
+    val thumbnail: String? = null,
+) {
+
+    fun toSManga(
+        apiUrl: String,
+        coverPath: String,
+    ): SManga = SManga.create().apply {
+        title = this@HeanCmsSearchDto.title
+        thumbnail_url = thumbnail?.toAbsoluteThumbnailUrl(apiUrl, coverPath)
+        url = "/series/$slug"
+    }
+}
+
+@Serializable
+data class HeanCmsSeriesDto(
+    val id: Int,
+    @SerialName("series_slug") val slug: String,
+    @SerialName("series_type") val type: String = "Comic",
+    val author: String? = null,
+    val description: String? = null,
+    val studio: String? = null,
+    val status: String? = null,
+    val thumbnail: String,
+    val title: String,
+    val tags: List<HeanCmsTagDto>? = emptyList(),
+    val chapters: List<HeanCmsChapterDto>? = emptyList(),
+    val seasons: List<HeanCmsSeasonsDto>? = emptyList(),
+) {
+
+    fun toSManga(
+        apiUrl: String,
+        coverPath: String,
+    ): SManga = SManga.create().apply {
+        val descriptionBody = this@HeanCmsSeriesDto.description?.let(Jsoup::parseBodyFragment)
+
+        title = this@HeanCmsSeriesDto.title
+        author = this@HeanCmsSeriesDto.author?.trim()
+        artist = this@HeanCmsSeriesDto.studio?.trim()
+        description = descriptionBody?.select("p")
+            ?.joinToString("\n\n") { it.text() }
+            ?.ifEmpty { descriptionBody.text().replace("\n", "\n\n") }
+        genre = tags.orEmpty()
+            .sortedBy(HeanCmsTagDto::name)
+            .joinToString { it.name }
+        thumbnail_url = thumbnail.ifEmpty { null }
+            ?.toAbsoluteThumbnailUrl(apiUrl, coverPath)
+        status = this@HeanCmsSeriesDto.status?.toStatus() ?: SManga.UNKNOWN
+        url = "/series/$slug"
+    }
+}
+
+@Serializable
+data class HeanCmsSeasonsDto(
+    val index: Int,
+    val chapters: List<HeanCmsChapterDto>? = emptyList(),
+)
+
+@Serializable
+data class HeanCmsTagDto(val name: String)
+
+@Serializable
+data class HeanCmsChapterDto(
+    val id: Int,
+    @SerialName("chapter_name") val name: String,
+    @SerialName("chapter_slug") val slug: String,
+    val index: String,
+    @SerialName("created_at") val createdAt: String,
+    val price: Int? = null,
+) {
+
+    fun toSChapter(seriesSlug: String, dateFormat: SimpleDateFormat): SChapter = SChapter.create().apply {
+        name = this@HeanCmsChapterDto.name.trim()
+        date_upload = runCatching { dateFormat.parse(createdAt)?.time }
+            .getOrNull() ?: 0L
+        url = "/series/$seriesSlug/$slug#$id"
+    }
+}
+
+@Serializable
+data class HeanCmsReaderDto(
+    val content: HeanCmsReaderContentDto? = null,
+)
+
+@Serializable
+data class HeanCmsReaderContentDto(
+    val images: List<String>? = emptyList(),
+)
+
+@Serializable
+data class HeanCmsQuerySearchPayloadDto(
+    val order: String,
+    val page: Int,
+    @SerialName("order_by") val orderBy: String,
+    @SerialName("series_status") val status: String? = null,
+    @SerialName("series_type") val type: String,
+    @SerialName("tags_ids") val tagIds: List<Int> = emptyList(),
+)
+
+@Serializable
+data class HeanCmsSearchPayloadDto(val term: String)
+
+private fun String.toAbsoluteThumbnailUrl(apiUrl: String, coverPath: String): String {
+    return if (startsWith("https://")) this else "$apiUrl/$coverPath$this"
+}
+
+fun String.toStatus(): Int = when (this) {
+    "Ongoing" -> SManga.ONGOING
+    "Hiatus" -> SManga.ON_HIATUS
+    "Dropped" -> SManga.CANCELLED
+    "Completed", "Finished" -> SManga.COMPLETED
+    else -> SManga.UNKNOWN
+}
---
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/heancms/HeanCmsGenerator.kt
@@ -0,0 +1,24 @@
+package eu.kanade.tachiyomi.multisrc.heancms
+
+import generator.ThemeSourceData.SingleLang
+import generator.ThemeSourceGenerator
+
+class HeanCmsGenerator : ThemeSourceGenerator {
+
+    override val themePkg = "heancms"
+
+    override val themeClass = "HeanCms"
+
+    override val baseVersionCode: Int = 16
+
+    override val sources = listOf(
+             SingleLang("YugenMangas", "https://yugenmangas.net", "es", isNsfw = true, overrideVersionCode = 6),
+    )
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            HeanCmsGenerator().createAll()
+        }
+    }
+}
