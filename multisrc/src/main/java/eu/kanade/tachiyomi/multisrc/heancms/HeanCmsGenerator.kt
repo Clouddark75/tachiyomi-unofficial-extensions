ccac7c15f0c2aa5859b82481d975ce76457fe98b
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2023-08-27 06:12:10 -0400

    Update HeanCmsGenerator.kt

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/heancms/HeanCmsGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/heancms/HeanCmsGenerator.kt
@@ -9,7 +9,7 @@ class HeanCmsGenerator : ThemeSourceGenerator {
 
     override val themeClass = "HeanCms"
 
-    override val baseVersionCode: Int = 17
+    override val baseVersionCode: Int = 18
 
     override val sources = listOf(
         SingleLang("YugenMangas", "https://yugenmangas.net", "es", isNsfw = true, overrideVersionCode = 6),
