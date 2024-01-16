75a83500af0714ae3317fbc67efc0e0060f34e13
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2024-01-16 07:31:12 -0400

    Conf

--- src/es/leercapitulo/src/eu/kanade/tachiyomi/extension/es/leercapitulo/LeerCapitulo.kt
+++ src/es/leercapitulo/src/eu/kanade/tachiyomi/extension/es/leercapitulo/LeerCapitulo.kt
@@ -1,18 +1,15 @@
 package eu.kanade.tachiyomi.extension.es.leercapitulo
 
-import android.app.Application
 import android.util.Base64
-import androidx.preference.ListPreference
-import androidx.preference.PreferenceScreen
-import eu.kanade.tachiyomi.extension.es.leercapitulo.dto.MangaDto
+import eu.kanade.tachiyomi.lib.synchrony.Deobfuscator
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.ConfigurableSource
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import okhttp3.HttpUrl.Companion.toHttpUrl
@@ -20,13 +17,10 @@ import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
 import uy.kohesive.injekt.injectLazy
 import java.nio.charset.Charset
-import kotlin.random.Random
 
-class LeerCapitulo : ParsedHttpSource(), ConfigurableSource {
+class LeerCapitulo : ParsedHttpSource() {
     override val name = "LeerCapitulo"
 
     override val lang = "es"
@@ -35,53 +29,34 @@ class LeerCapitulo : ParsedHttpSource(), ConfigurableSource {
 
     private val json: Json by injectLazy()
 
-    private val isCi = System.getenv("CI") == "true"
+    override val baseUrl = "https://www.leercapitulo.com"
 
-    override val baseUrl
-        get() = when {
-            isCi -> MIRRORS.joinToString("#, ")
-            else -> _baseUrl
-        }
+    override fun headersBuilder() = super.headersBuilder()
+        .add("Referer", "$baseUrl/")
 
-    private val _baseUrl = run {
-        val preferences = Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
-        val mirrors = MIRRORS
-        var index = preferences.getString(MIRROR_PREF, "-1")!!.toInt()
-        if (index !in mirrors.indices) {
-            index = Random.nextInt(0, mirrors.size)
-            preferences.edit().putString(MIRROR_PREF, index.toString()).apply()
-        }
-        mirrors[index]
-    }
+    override fun popularMangaRequest(page: Int): Request = GET(baseUrl, headers)
 
-    // Popular
-    override fun popularMangaRequest(page: Int): Request =
-        GET(baseUrl, headers)
-
-    override fun popularMangaSelector(): String =
-        ".hot-manga > .thumbnails > a"
+    override fun popularMangaSelector(): String = ".hot-manga > .thumbnails > a"
 
     override fun popularMangaFromElement(element: Element): SManga = SManga.create().apply {
-        setUrlWithoutDomain(createMangaUrl(element.attr("abs:href")))
+        setUrlWithoutDomain(element.attr("abs:href"))
         title = element.attr("title")
-        thumbnail_url = element.selectFirst("img")!!.attr("abs:src")
+        thumbnail_url = element.selectFirst("img")!!.imgAttr()
     }
 
-    override fun popularMangaNextPageSelector(): String? =
-        null
+    override fun popularMangaNextPageSelector(): String? = null
 
-    // Search
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         val url = "$baseUrl/search-autocomplete".toHttpUrl().newBuilder()
             .addQueryParameter("term", query)
 
-        return GET(url.toString(), headers)
+        return GET(url.build(), headers)
     }
 
     override fun searchMangaParse(response: Response): MangasPage {
         val mangas = json.decodeFromString<List<MangaDto>>(response.body.string()).map {
             SManga.create().apply {
-                setUrlWithoutDomain(createMangaUrl(it.link))
+                setUrlWithoutDomain(it.link)
                 title = it.label
                 thumbnail_url = baseUrl + it.thumbnail
             }
@@ -90,43 +65,24 @@ class LeerCapitulo : ParsedHttpSource(), ConfigurableSource {
         return MangasPage(mangas, hasNextPage = false)
     }
 
-    override fun searchMangaSelector(): String =
-        throw UnsupportedOperationException("Not used.")
+    override fun searchMangaSelector(): String = throw UnsupportedOperationException()
 
-    override fun searchMangaFromElement(element: Element): SManga =
-        throw UnsupportedOperationException("Not used.")
+    override fun searchMangaFromElement(element: Element): SManga = throw UnsupportedOperationException()
 
-    override fun searchMangaNextPageSelector(): String? =
-        null
+    override fun searchMangaNextPageSelector(): String? = null
 
-    // Latest
-    override fun latestUpdatesRequest(page: Int): Request =
-        popularMangaRequest(page)
+    override fun latestUpdatesRequest(page: Int): Request = popularMangaRequest(page)
 
-    override fun latestUpdatesSelector(): String =
-        ".mainpage-manga"
+    override fun latestUpdatesSelector(): String = ".mainpage-manga"
 
     override fun latestUpdatesFromElement(element: Element): SManga = SManga.create().apply {
-        val url = element.selectFirst(".media-body > a")!!.attr("abs:href")
-        setUrlWithoutDomain(createMangaUrl(url))
+        setUrlWithoutDomain(element.selectFirst(".media-body > a")!!.attr("abs:href"))
         title = element.selectFirst("h4")!!.text()
-        thumbnail_url = element.selectFirst("img")!!.attr("abs:src")
+        thumbnail_url = element.selectFirst("img")!!.imgAttr()
     }
 
-    override fun latestUpdatesNextPageSelector(): String? =
-        null
+    override fun latestUpdatesNextPageSelector(): String? = null
 
-    private fun createMangaUrl(urlString: String): String {
-        val urlPath = urlString.substringAfter(baseUrl)
-        val pathParts = urlPath.removeSurrounding("/").split("/")
-        return if (pathParts.size > 2) {
-            "$baseUrl/${pathParts[0]}/${pathParts[2]}/"
-        } else {
-            urlString
-        }
-    }
-
-    // Details
     override fun mangaDetailsParse(document: Document): SManga = SManga.create().apply {
         title = document.selectFirst("h1")!!.text()
 
@@ -139,45 +95,64 @@ class LeerCapitulo : ParsedHttpSource(), ConfigurableSource {
 
         genre = document.select(".description-update a[href^='/genre/']").joinToString { it.text() }
         status = document.selectFirst(".description-update > span:contains(Estado:) + :matchText")!!.text().toStatus()
-        thumbnail_url = document.selectFirst(".cover-detail > img")!!.attr("abs:src")
+        thumbnail_url = document.selectFirst(".cover-detail > img")!!.imgAttr()
     }
 
-    // Chapters
-    override fun chapterListSelector(): String =
-        ".chapter-list > ul > li"
+    override fun chapterListSelector(): String = ".chapter-list > ul > li"
 
     override fun chapterFromElement(element: Element): SChapter = SChapter.create().apply {
-        val a = element.selectFirst("a.xanh")!!
-        setUrlWithoutDomain(a.attr("abs:href"))
-        name = a.text()
+        with(element.selectFirst("a.xanh")!!) {
+            setUrlWithoutDomain(attr("abs:href"))
+            name = text()
+        }
     }
 
-    // Pages
     override fun pageListParse(document: Document): List<Page> {
         val orderList = document.selectFirst("meta[property=ad:check]")?.attr("content")
             ?.replace("[^\\d]+".toRegex(), "-")
             ?.split("-")
 
-        val arrayData = document.selectFirst("#arraydata")!!.text()
+        val useReversedString = orderList?.any { it == "01" }
+
+        val arrayData = document.selectFirst("#array_data")!!.text()
+
+        val scriptUrl = document.selectFirst("section.bodycontainer > script[src$=.js]")?.attr("abs:src")
+            ?: throw Exception("Script not found")
+
+        val scriptData = client.newCall(GET(scriptUrl, headers)).execute().body.string()
+
+        val deobfuscatedScript = Deobfuscator.deobfuscateScript(scriptData)
+            ?: throw Exception("Unable to deobfuscate script")
+
+        val keyRegex = """'([A-Z0-9]{62})'""".toRegex(RegexOption.IGNORE_CASE)
+
+        val (key1, key2) = keyRegex.findAll(deobfuscatedScript).map { it.groupValues[1] }.toList()
 
         val encodedUrls = arrayData.replace(Regex("[A-Z0-9]", RegexOption.IGNORE_CASE)) {
-            val index = "ZOPo0z9kmYrtdQ8GweChFygXJBsvTSulUKp2LWqEjDRN5c7V34naA6M1fbIxHi".indexOf(it.value)
-            "sPYv8dyzOtFGJhVekLQNRD1m5rX2HSjo9quf6xU0TB7cIbCi3MlwgKnEWAZa4p"[index].toString()
+            val index = key2.indexOf(it.value)
+            key1[index].toString()
         }
 
         val urlList = String(Base64.decode(encodedUrls, Base64.DEFAULT), Charset.forName("UTF-8")).split(",")
 
-        val sortedUrls = orderList?.map { urlList[it.reversed().toInt()] }?.reversed() ?: urlList
+        val sortedUrls = orderList?.map {
+            if (useReversedString == true) urlList[it.reversed().toInt()] else urlList[it.toInt()]
+        }?.reversed() ?: urlList
 
         return sortedUrls.mapIndexed { i, image_url ->
             Page(i, imageUrl = image_url)
         }
     }
 
-    override fun imageUrlParse(document: Document): String =
-        throw UnsupportedOperationException("Not used.")
+    private fun Element.imgAttr(): String = when {
+        hasAttr("data-lazy-src") -> attr("abs:data-lazy-src")
+        hasAttr("data-src") -> attr("abs:data-src")
+        else -> attr("abs:src")
+    }
 
-    // Other
+    override fun imageUrlParse(document: Document): String =
+        throw UnsupportedOperationException()
+
     private fun String.toStatus() = when (this) {
         "Ongoing" -> SManga.ONGOING
         "Paused" -> SManga.ON_HIATUS
@@ -186,20 +161,11 @@ class LeerCapitulo : ParsedHttpSource(), ConfigurableSource {
         else -> SManga.UNKNOWN
     }
 
-    override fun setupPreferenceScreen(screen: PreferenceScreen) {
-        ListPreference(screen.context).apply {
-            val mirrors = MIRRORS
-
-            key = MIRROR_PREF
-            title = "Mirror"
-            summary = "%s\nRequires restart to take effect"
-            entries = mirrors
-            entryValues = Array(mirrors.size, Int::toString)
-        }.let(screen::addPreference)
-    }
-
-    companion object {
-        private const val MIRROR_PREF = "MIRROR"
-        private val MIRRORS get() = arrayOf("https://www.leercapitulo.com", "https://olympusscan.top")
-    }
+    @Serializable
+    data class MangaDto(
+        val label: String,
+        val link: String,
+        val thumbnail: String,
+        val value: String,
+    )
 }
